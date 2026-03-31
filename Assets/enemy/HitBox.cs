44c85249ee94ead4218636c3a99d493b5fc76898
Yunara0717 <97178147+Yunara0717@users.noreply.github.com>
2026-03-31 12:33:39 +0900

    나머지

--- Assets/enemy/HitBox.cs
+++ Assets/enemy/HitBox.cs
@@ -11,24 +11,14 @@ public class HitBox : MonoBehaviour, IDamageable
         if (transform.parent != null)
         {
             parentObject = transform.parent.GetComponentInParent<IDamageable>();
-            if (parentObject != null)
-            {
-                Debug.Log($"[HitBox] 나랑 연결된 진짜 보스 찾음: {((MonoBehaviour)parentObject).gameObject.name} ! 야호!");
-            }
-            else
-            {
-                Debug.LogWarning("[HitBox] 에러! 내 부모 중에 보스(IDamageable)가 안 보여 ㅠㅠ");
-            }
         }
     }
 
         public void TakeDamage(float damage)
         {
-            Debug.Log($"[HitBox 탐정] 칼에 찔렸음! 데미지: {damage}");
             if (parentObject != null)
             {
                 parentObject.TakeDamage(damage);
-                Debug.Log("[HitBox] 부모에게 전달 완료!");
             }
         }
 
