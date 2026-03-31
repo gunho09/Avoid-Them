e1e12abb18b2796ae8043a843f8ff8d33716fac2
Yunara0717 <97178147+Yunara0717@users.noreply.github.com>
2026-03-31 12:28:36 +0900

    보스1콜라이더

--- Assets/enemy/HitBox.cs
+++ Assets/enemy/HitBox.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 using System.Collections;
-public class HitBox : MonoBehaviour
+using UnityEngine.InputSystem.iOS;
+public class HitBox : MonoBehaviour, IDamageable
+#pragma warning restore format
 {
     private IDamageable parentObject;
 
-        void Start()
+    void Start()
+    {
+        if (transform.parent != null)
         {
-            parentObject = GetComponentInParent<IDamageable>();
+            parentObject = transform.parent.GetComponentInParent<IDamageable>();
+            if (parentObject != null)
+            {
+                Debug.Log($"[HitBox] 나랑 연결된 진짜 보스 찾음: {((MonoBehaviour)parentObject).gameObject.name} ! 야호!");
+            }
+            else
+            {
+                Debug.LogWarning("[HitBox] 에러! 내 부모 중에 보스(IDamageable)가 안 보여 ㅠㅠ");
+            }
         }
+    }
 
         public void TakeDamage(float damage)
         {
-            Debug.Log($"[HitBox] 맞음! 데미지: {damage}");
+            Debug.Log($"[HitBox 탐정] 칼에 찔렸음! 데미지: {damage}");
             if (parentObject != null)
             {
                 parentObject.TakeDamage(damage);
@@ -19,4 +32,12 @@ public class HitBox : MonoBehaviour
             }
         }
 
+        public float GetHpRatio()
+        {
+            if (parentObject != null)
+            {
+                return parentObject.GetHpRatio();
+            }
+            return 1f;
+        }
 }
